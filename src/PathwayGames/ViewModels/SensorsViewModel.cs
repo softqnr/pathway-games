5fd059ffab9f4c4052e3baebdaadc3e04c0d4f9e
Stelios Fradelakis <sfradel@gmail.com>
2020-09-29 09:25:09 +0300

    Removed Oxyplot package

--- src/PathwayGames/ViewModels/SensorsViewModel.cs
+++ src/PathwayGames/ViewModels/SensorsViewModel.cs
@@ -1,7 +1,4 @@
-﻿using OxyPlot;
-using OxyPlot.Axes;
-using OxyPlot.Series;
-using PathwayGames.Models;
+﻿using PathwayGames.Models;
 using PathwayGames.Sensors;
 using PathwayGames.Services.Engangement;
 using PathwayGames.Services.Sensors;
@@ -18,8 +15,6 @@ namespace PathwayGames.ViewModels
     public class SensorsViewModel : ViewModelBase
     {
         private const int PlotUpdateInterval = 20;
-        private const int NumberOfSeries = 1;
-        private bool Disposed;
         private readonly Timer timer;
         private readonly Stopwatch watch = new Stopwatch();
         //Services
@@ -27,14 +22,7 @@ namespace PathwayGames.ViewModels
         private readonly IUserService _userService;
         private readonly IEngangementService _engangementService;
 
-        private PlotModel _plotModel;
         public UserGameSettings _userSettings;
-
-        public PlotModel PlotModel
-        {
-            get => _plotModel;
-            private set => SetProperty(ref _plotModel, value);
-        }
 
         public UserGameSettings UserSettings
         {
@@ -76,46 +64,7 @@ namespace PathwayGames.ViewModels
 
         private void CreatePlot()
         {
-            PlotModel = new PlotModel();
-
-            PlotModel.Axes.Add(new LinearAxis
-            {
-                Position = AxisPosition.Left,
-                MajorGridlineStyle = LineStyle.Dot,
-                Minimum = 0,
-                Maximum = 100,
-                AbsoluteMinimum = 0,
-                AbsoluteMaximum = 100,
-                MinorStep = 100,
-                MajorStep = 10
-            });
-            
-            //DateTimeAxis dtx = new DateTimeAxis
-            //{
-            //    Position = AxisPosition.Bottom,
-            //    MajorGridlineStyle = LineStyle.Dot,
-            //    AbsoluteMinimum = DateTimeAxis.ToDouble(batteryGraph.Min(v => v.LogTimeStamp)) - (0.5d / 60),
-            //    MajorStep = 5d,
-            //    StringFormat = "MM-dd-yy",
-            //    AbsoluteMaximum = DateTimeAxis.ToDouble(batteryGraph.Max(v => v.LogTimeStamp)) + (0.5d / 6),
-            //    Minimum = DateTimeAxis.ToDouble(batteryGraph.Min(v => v.LogTimeStamp)) - (0.5d / 60),
-            //    Maximum = DateTimeAxis.ToDouble(batteryGraph.Max(v => v.LogTimeStamp)) + (0.5d / 6)
-            //};
-            //plotModel.Axes.Add(dtx);
-
-            var areaSerie = new AreaSeries
-            {
-                StrokeThickness = 2.0
-            };
-
-            areaSerie.Points.Add(new DataPoint(0, 50));
-            areaSerie.Points.Add(new DataPoint(10, 60));
-            areaSerie.Points.Add(new DataPoint(20, 140));
-            areaSerie.Points2.Add(new DataPoint(0, 50));
-            areaSerie.Points2.Add(new DataPoint(5, 70));
-            areaSerie.Points2.Add(new DataPoint(15, 60));
-
-            PlotModel.Series.Add(areaSerie);
+ 
         }
 
         private void Update()
